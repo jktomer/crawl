fad7f95ea26c6fd4fa9d91e3f8779dea819e6a3d
Edgar A. Bering IV <trizor@gmail.com>
2021-12-04 10:47:04 +0200

    fix: increase enslave soul duration
    Now that it's a bit more awkward to use, give players a little more time
    to make the kill, since the zombies are paid out either way.

--- crawl-ref/source/ability.cc
+++ crawl-ref/source/ability.cc
@@ -2826,7 +2826,7 @@ static spret _do_ability(const ability_def& abil, bool fail, dist *target)
         fail_check();
 
         pay_yred_souls(soul_cost);
-        const int duration = you.skill_rdiv(SK_INVOCATIONS, 3, 4) + 2;
+        const int duration = you.skill_rdiv(SK_INVOCATIONS, 3, 2) + 4;
         mons->add_ench(mon_enchant(ENCH_SOUL_RIPE, 0, &you,
                                    duration * BASELINE_DELAY));
         simple_monster_message(*mons, "'s soul is now ripe for the taking.");
